390c627ca468d01ab3bd5e79a62344c32e30ce56
aokomoriuta_lab <internal@enmps.net>
2012-09-07 12:46:14 +0900

    Rで行列の可視化

--- R/CG.R
+++ R/CG.R
@@ -1,51 +1,58 @@
-N <- 10
+N <- 21
+M <- 6
 
 A <- matrix(0, nrow=N, ncol=N)
 x <- numeric(N)
 b <- numeric(N)
 
-for (i in 0:(N-2))
+for (i in 0:(N-1))
 {
-	for (j in (i+1):(N-1))
+	for (j in (max(0, i - M/2+1)):(min(N, i + M/2)-1))
 	{
-		if ((i - j)^2 < 3*3)
+		if(i != j)
 		{
-			a_ij <-abs(sin(i*i + 2*j))
-
+			a_ij <-abs(sin(i + j))
 			A[i+1, j+1] <- a_ij
-			A[j+1, i+1] <- a_ij
 
 			A[i+1,i+1] <- A[i+1,i+1] + a_ij
-			A[j+1,j+1] <- A[j+1,j+1] + a_ij
 		}
 	}
 
-	x[i+1] <- 0
+	x[i+1] <- i/10
 
-	b[i+1] <- cos(i*i*0.1)
+	b[i+1] <- cos(i) * 10
 }
 print(A)
-print(b)
+#hist(eigen(A)$values)
+#kappa(A)
+
+i <- floor((which(A > 0)-1)/N)
+j <- (which(A > 0)-1)%%N
+
+plot(j, i)
 
 solve(A, b)
+
+
 
 Ap <- A %*% x
 p <- b - Ap
 r <- p
 
-rr <- 1
+residual <- 1
 
-#while(rr > 1e-12)
+#while(residual > 1e-4)
 {
 	rr <- (t(r) %*% r)[1,1]
 	Ap <- A %*% p
-	print(Ap)
 	alpha = rr / (t(p) %*% Ap)[1,1]
 	x <- x + alpha*p
 	r <- r - alpha*Ap
+	residual = max(abs(r))
+	print(r)
+
+
 	rrNew <- (t(r) %*% r)[1,1]
-
-
 	beta <- rrNew / rr
 	p <- r + beta*p
 }
